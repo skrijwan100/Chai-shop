52697adc87b49de6ee0cf690f57aaf38c3510429
skrijwan <skrijwan329@gmail.com>
2025-02-07 20:58:17 +0530

    add footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-      <nav>
+      <nav className='flex justify-between text-white bg-black h-16 items-center px-7'>
+        <div className='font-bold'> This is logo </div>
         <ul className='flex gap-3'>
             <li>Home</li>
             <li>about</li>
-            <li>servis</li>
-            <li>contact</li>
+            <li>service</li>
+            <li>Login</li>
+            <li>Sing up</li>
         </ul>
       </nav>
     </div>
