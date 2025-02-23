bf2ff370703d10300cf7a38537e8409abf0852da
skrijwan <skrijwan329@gmail.com>
2025-02-23 12:11:51 +0530

    add google auth

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,8 +7,10 @@ export default function Navbar() {
   const { data: session } = useSession()
   if(session) {
     return <>
-      Signed in as {session.user.email} <br/>
-      <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => signOut()}>Sign out</button>
+      {/* Signed in as {session.user.email} <br/> */}
+      <div className=' bg-[#002439]  h-16 flex justify-end'>
+      <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => signOut()}>LogOut</button>
+      </div>
     </>
   }
   return (
